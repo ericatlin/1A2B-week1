ffccf647dbdd4a4e8aeb79fae3d7d9aff50edd98
林智珉 <eriksonop@gmail.com>
2023-02-18 23:46:58 +0800

    test

--- 1A2B/Program.cs
+++ 1A2B/Program.cs
@@ -14,49 +14,73 @@ namespace _1A2B
             
             //宣告變數
 
-            Random rnd = new Random();
-
-            for (int i = 0; i < 4; i++)
+            while (true)
             {
-                arryRndnum[i] = Convert.ToString(rnd.Next(0, 9));
-
-                if (arryRndnum[0] == "0")
+                Random rnd = new Random();
+                int a = 0, b = 0;
+                for (int i = 0; i < 4; i++)
                 {
-                    arryRndnum[0] = Convert.ToString(rnd.Next(1, 9));
-                }
-                //第一位數字不為零
+                    arryRndnum[i] = Convert.ToString(rnd.Next(0, 9));
 
-                for (int j = 0; j < i; j++)
-                {
-                    if (arryRndnum[j] == arryRndnum[i])
+                    if (arryRndnum[0] == "0")
                     {
-                        j = 0;
-                        arryRndnum[i] = Convert.ToString(rnd.Next(0, 9));
+                        arryRndnum[0] = Convert.ToString(rnd.Next(1, 9));
                     }
-                }
-                Console.WriteLine(arryRndnum[i]);
-            }
-            //亂數
-            
-            Console.WriteLine("請輸入4個不重複的數字");
-            string plyNum = Console.ReadLine();
-            string[] arrPlyNum = new string[4];
-            for (int i = 0; i < 4; i++)
-            {
-                arrPlyNum[i] = Convert.ToString(plyNum[i]);
-            }
-            var intersect = arrPlyNum.Intersect(arryRndnum);
-            Console.WriteLine("聯集的結果為");
-            string[] a = new string[4];
-            int k = 0;
-            foreach (var item in intersect)
-            {
-                var person2 = arryRndnum.First((x) => x == item);
-                Console.WriteLine($"小於30歲的人第一個被找到的是:{person2}");
-            }
-            
+                    //第一位數字不為零
 
-            Console.ReadLine();
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (arryRndnum[j] == arryRndnum[i])
+                        {
+                            j = 0;
+                            arryRndnum[i] = Convert.ToString(rnd.Next(0, 9));
+                        }
+                    }
+                    Console.WriteLine(arryRndnum[i]);
+                }
+                //亂數
+                Console.WriteLine("歡迎來到1A2B猜數字遊戲~");
+
+                for (int z = 1; z != 0; z++)
+                {
+                    Console.WriteLine("請輸入4個不重複的數字");
+                    string plyNum = Console.ReadLine();
+                    string[] arrPlyNum = new string[4];
+                    for (int i = 0; i < 4; i++)
+                    {
+                        arrPlyNum[i] = Convert.ToString(plyNum[i]);
+                    }
+                    var intersect = arrPlyNum.Intersect(arryRndnum);
+
+
+                    foreach (var item in intersect)
+                    {
+
+                        if (Array.IndexOf(arryRndnum, item) == Array.IndexOf(arrPlyNum, item))
+                        {
+                            a++;
+                        }
+                        else
+                        {
+                            b++;
+                        }
+                    }
+                    Console.WriteLine($"判定結果是 {a}A{b}B");
+                    if (a == 4)
+                    {
+                        Console.WriteLine("恭喜你!猜對了!!!");
+                        break;
+                    }
+                    a = 0; b = 0;
+                }
+                Console.WriteLine("是否要繼續遊玩?(y/n):");
+                string yn = Console.ReadLine();
+                if (yn == "n")
+                {
+                    break;
+                }
+                Console.ReadKey();
+            }
         }
         
     }
